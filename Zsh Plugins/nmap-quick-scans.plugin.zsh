a2d45849ce63beb99454a860d8dfad31b574e552
Lawliet <jerry_howl_lawliet@hotmail.com>
2022-06-24 20:09:24 -0500

    Update nmap-quick-scans.plugin.zsh

--- Zsh Plugins/nmap-quick-scans.plugin.zsh
+++ Zsh Plugins/nmap-quick-scans.plugin.zsh
@@ -3,6 +3,15 @@
 # $1 --> 127.0.0.1
 function nmapenum() {sudo nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn $1 -oG allPorts}
 
+# $1 --> 127.0.0.1
+function nmapenumt() {sudo nmap -p- --open -sT --min-rate 5000 -vvv -n -Pn $1 -oG allPorts}
+
 # $1 --> 21,22,80
 # $2 --> 127.0.0.1
-function nmapscan() {sudo nmap -sCV -p$1 $2 -oN targeted}
+function nmapscan() {sudo nmap -sCV -p$1 -Pn $2 -oN targeted}
+
+# $1 --> 127.0.0.1
+function nmapwebscan() {nmap --script http-enum -p 80,$2 $1 -oN webScan}
+
+# $1 --> 127.0.0.1
+function nmapsslscan() {nmap --script "vuln and safe" -p 443,$2 $1 -oN sslScan}
