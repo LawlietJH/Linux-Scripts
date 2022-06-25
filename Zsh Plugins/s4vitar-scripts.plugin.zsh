27299049d729a8165d7df848098ff4a545c5c7de
Lawliet <jerry_howl_lawliet@hotmail.com>
2022-06-24 20:01:27 -0500

    Update s4vitar-scripts.plugin.zsh

--- Zsh Plugins/s4vitar-scripts.plugin.zsh
+++ Zsh Plugins/s4vitar-scripts.plugin.zsh
@@ -1,23 +1,49 @@
-# mkt: Make directories for CTF.
+# mkt: Creación de carpetas para hacer CTFs:
 function mkt(){
-	if [[ -n $1 ]]; then
-		mkdir $1
-		cd $1
+	DIR=""
+	CHALLENGE=false
+	SHIFTED=()
+	# Extraccion de Parametros
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+			-c|--challenge) CHALLENGE=true; shift;;
+			*) SHIFTED+=("$1"); shift;;
+		esac
+	done
+	DIR=${SHIFTED[1]}
+	if [[ -n $DIR ]]; then
+		mkdir $DIR
+		cd $DIR
 	fi
-	mkdir {nmap,content,exploits,scripts}
+	if [[ $CHALLENGE == true ]]; then
+		echo '# Challenge: '$DIR > data.txt
+		echo '# Challenge: '$DIR > flag.txt
+		echo 'Flag: ' >> flag.txt
+	else
+		mkdir {content,exploits,nmap}
+		cd content
+		echo '# Machine: '$DIR > data.txt
+		echo '# Machine: '$DIR > flags.txt
+		echo 'User Flag: ' >> flags.txt
+		echo 'Root Flag: ' >> flags.txt
+	fi
 }
 
 # Extract nmap information:
 function extractPorts(){
 	ports="$(cat $1 | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
 	ip_address="$(cat $1 | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u | head -n 1)"
-	echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
-	echo -e "\t[*] IP Address: $ip_address" >> extractPorts.tmp
-	echo -e "\t[*] Open ports: $ports\n" >> extractPorts.tmp
-	echo $ports | tr -d '\n' | xclip -sel clip
-	echo -e "[*] Ports copied to clipboard\n" >> extractPorts.tmp
-	cat extractPorts.tmp
-	rm extractPorts.tmp
+	if [[ $2 == '-s' ]]; then
+		echo $ports
+	else
+		echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
+		echo -e "\t[*] IP Address: $ip_address" >> extractPorts.tmp
+		echo -e "\t[*] Open ports: $ports\n" >> extractPorts.tmp
+		echo $ports | tr -d '\n' | xclip -sel clip
+		echo -e "[*] Ports copied to clipboard\n" >> extractPorts.tmp
+		bat extractPorts.tmp -l java
+		rm extractPorts.tmp
+	fi
 }
 
 alias extractports='extractPorts'
