b74ec92d5f68b2b5981428d3725a00cabbf1fd38
Lawliet <jerry_howl_lawliet@hotmail.com>
2022-06-30 14:17:30 -0500

    Update target-functions.plugin.zsh
    Función 'gettarget' mejorada.

--- Zsh Plugins/target-functions.plugin.zsh
+++ Zsh Plugins/target-functions.plugin.zsh
@@ -22,6 +22,33 @@ function getTarget(){
         temp2=$(cat ~/target.tmp | awk '{print $2}')
         ip_address=""
         machine_name=""
+        IP=false
+        NAME=false
+        SILENT=false
+        HELP=false
+        LAST=""
+        SHIFTED=()
+        # Extraccion de Parametros ---------------------------------------------
+        while [[ $# -gt 0 ]]; do
+            case $1 in
+                -i|-ip|--ip|ip) IP=true; LAST="IP"; shift;;
+                -n|-name|--name|name|-m|-machine|--machine|machine) NAME=true; LAST="NAME"; shift;;
+                -s|-silent|--silent|silent) SILENT=true; shift;;
+                -is|-si|-ips|-sip) IP=true; SILENT=true; LAST="IP"; shift;;
+                -ns|-sn|-ms|-sm) NAME=true; SILENT=true; LAST="NAME"; shift;;
+                -in|-im|-ipn|-ipm) IP=true; NAME=true; LAST="NAME"; shift;;
+                -ni|-mi|-nip|-mip) IP=true; NAME=true; LAST="IP"; shift;;
+                -sin|-sim|-sipn|-sipm) IP=true; NAME=true; SILENT=true; LAST="NAME"; shift;;
+                -sni|-smi|-snip|-smip) IP=true; NAME=true; SILENT=true; LAST="IP"; shift;;
+                -isn|-ism|-ipsn|-ipsm) IP=true; NAME=true; SILENT=true; LAST="NAME"; shift;;
+                -nsi|-msi|-nsip|-msip) IP=true; NAME=true; SILENT=true; LAST="IP"; shift;;
+                -ins|-ims|-ipns|-ipms) IP=true; NAME=true; SILENT=true; LAST="NAME"; shift;;
+                -nis|-mis|-nips|-mips) IP=true; NAME=true; SILENT=true; LAST="IP"; shift;;
+                -h|-help|--help|help) HELP=true; shift;;
+                *) SHIFTED+=("$1"); shift;;
+            esac
+        done
+        # Validación de IP -----------------------------------------------------
         isValidIP $temp1
         isvalid=$?
         if [[ $isvalid -eq 1 ]]; then
@@ -37,29 +64,52 @@ function getTarget(){
         if [[ $isvalid -eq 1 ]]; then
                 ip_address=$temp2
         fi
-        if [[ -n $1 ]]; then
-                if [[ $1 == "-ip" || $1 == "--ip" || $1 == "-i" ]]; then
-                        if [[ -n $ip_address ]]; then
-                                echo -n "$ip_address" | xclip -sel clip
-                                echo "Copied To Clipboard: $ip_address"
-                        else
-                                echo "No Target IP"
+        # ----------------------------------------------------------------------
+        if [[ $HELP == true ]]; then
+                echo "Usage: gettarget [-h] | [-ip -name -silent | -ins]"
+                echo "    -i, ip       Copia la IP del 'target' en el Clipboard (Ver: settarget)"
+                echo "    -n, name     Copia el nombre del 'target' en el Clipboard (Ver: settarget)"
+                echo "    -s, silent   Muestra la IP/Nombre del 'Target'. Ejemplo: echo 'Target IP:' \$(gettarget -s)"
+                echo "    -h, help     Muestra esta ayuda."
+                echo .
+                echo "Ejemplos:"
+                echo "    gettarget                 # Copied To Clipboard: 127.0.0.1"
+                echo "    gettarget name            # Copied To Clipboard: localhost"
+                echo "    gettarget -s              # 127.0.0.1"
+                echo "    gettarget -sn             # localhost"
+                echo "    gettarget ip name silent  # 127.0.0.1 localhost"
+                echo "    gettarget name ip -s      # localhost 127.0.0.1"
+                echo "    gettarget -i -n           # Copied To Clipboard: 127.0.0.1 localhost"
+                echo "    gettarget -ni             # Copied To Clipboard: localhost 127.0.0.1"
+        elif [[ -n $SHIFTED ]]; then
+                echo "Error: $SHIFTED"
+        elif [[ $SILENT == true ]]; then
+                if [[ -n $ip_address && -n $machine_name && $IP == true && $NAME == true ]]; then
+                        if [[ $LAST == "NAME" ]]; then
+                                echo "$ip_address $machine_name"
+                        elif [[ $LAST == "IP" ]]; then
+                                echo "$machine_name $ip_address"
                         fi
-                elif [[ $1 == "-name" || $1 == "--name" || $1 == "-n" ]]; then
-                        if [[ -n $machine_name ]]; then
-                                echo -n "$machine_name" | xclip -sel clip
-                                echo "Copied To Clipboard: $machine_name"
-                        else
-                                echo "No Target Name"
-                        fi
+                elif [[ -n $ip_address && $NAME == false ]] || [[ -n $ip_address && $IP == true ]]; then
+                        echo "$ip_address"
+                elif [[ -n $machine_name && $IP == false ]] || [[ -n $machine_name && $NAME == true ]]; then
+                        echo "$machine_name"
                 else
-                        echo "Error: $1"
+                        echo "No Target"
                 fi
         else
-                if [[ $ip_address ]]; then
+                if [[ -n $ip_address && -n $machine_name && $IP == true && $NAME == true ]]; then
+                        if [[ $LAST == "NAME" ]]; then
+                                echo -n "$ip_address $machine_name" | xclip -sel clip
+                                echo "Copied To Clipboard: $ip_address $machine_name"
+                        elif [[ $LAST == "IP" ]]; then
+                                echo -n "$machine_name $ip_address" | xclip -sel clip
+                                echo "Copied To Clipboard: $machine_name $ip_address"
+                        fi
+                elif [[ -n $ip_address && $NAME == false ]] || [[ -n $ip_address && $IP == true ]]; then
                         echo -n "$ip_address" | xclip -sel clip
                         echo "Copied To Clipboard: $ip_address"
-                elif [[ $machine_name ]]; then
+                elif [[ -n $machine_name && $IP == false ]] || [[ -n $machine_name && $NAME == true ]]; then
                         echo -n "$machine_name" | xclip -sel clip
                         echo "Copied To Clipboard: $machine_name"
                 else
