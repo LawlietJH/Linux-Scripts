802454ca51ccd9b0b232bf9fb18f1378c4eca0bb
Lawliet <jerry_howl_lawliet@hotmail.com>
2022-07-06 16:04:58 -0500

    Update nmap-quick-scans.plugin.zsh

--- Zsh Plugins/nmap-quick-scans.plugin.zsh
+++ Zsh Plugins/nmap-quick-scans.plugin.zsh
@@ -6,12 +6,29 @@ function nmapenum() {sudo nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn $1 -oG
 # $1 --> 127.0.0.1
 function nmapenumt() {sudo nmap -p- --open -sT --min-rate 5000 -vvv -n -Pn $1 -oG allPorts}
 
-# $1 --> 21,22,80
+# $1 --> 127.0.0.1
+function nmapenumu() {sudo nmap -p- --open -sU --min-rate 5000 -vvv -n -Pn $1 -oG allPorts}
+
+# $1 --> 21,22,80...
 # $2 --> 127.0.0.1
 function nmapscan() {sudo nmap -sCV -p$1 -Pn $2 -oN targeted}
 
 # $1 --> 127.0.0.1
-function nmapwebscan() {nmap --script http-enum -p 80,$2 $1 -oN webScan}
+# $2 --> 80,8080...
+function nmapwebscan() {
+	if [[ -z $2 ]]; then
+		nmap --script http-enum -p 80 $1 -oN webScan
+	else
+		nmap --script http-enum -p $2 $1 -oN webScan
+	fi
+}
 
 # $1 --> 127.0.0.1
-function nmapsslscan() {nmap --script "vuln and safe" -p 443,$2 $1 -oN sslScan}
+# $2 --> 443,54321...
+function nmapsslscan() {
+	if [[ -z $2 ]]; then
+		nmap --script "vuln and safe" -p 443 $1 -oN sslScan
+	else
+		nmap --script "vuln and safe" -p $2 $1 -oN sslScan
+	fi
+}
